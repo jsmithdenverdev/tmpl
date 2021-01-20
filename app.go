da33feaf45368f66d4f256116df84dfcb3232f0d
Jacob Smith <jsmithdenverdev@gmail.com>
2021-01-20 20:08:17 +0000

    got rid of parseInput, changed methods to hang off app

--- app.go
+++ app.go
@@ -18,26 +18,28 @@ type app struct {
 }
 
 func (a *app) run() error {
-	// read inputPath
-	buf, err := readInput(a.config.readFromStdin, a.config.inputPath)
+	reader, err := a.createInputReader()
 	if err != nil {
 		return err
 	}
 
+	buf, err := ioutil.ReadAll(reader)
+
 	// parse inputPath
-	templateData, err := parseInput(buf)
+	templateData := map[string]interface{}{}
+	err = json.Unmarshal(buf, &templateData)
 	if err != nil {
 		return err
 	}
 
 	// parse templates
-	tmpl, err := parseTemplates(a.config.templatePaths, a.config.templateStrings)
+	tmpl, err := a.parseTemplates()
 	if err != nil {
 		return err
 	}
 
 	// create writer
-	writer, err := createWriter(a.config.writeToStdout, a.config.outputPath)
+	writer, err := a.createOutputWriter()
 	if err != nil {
 		return err
 	}
@@ -51,33 +53,28 @@ func (a *app) run() error {
 	return nil
 }
 
-func readInput(readFromStdin bool, inputPath string) ([]byte, error) {
-	// if inputPath was piped into the CLI it will be read off os.Stdin
-	if readFromStdin {
-		buf, err := ioutil.ReadAll(os.Stdin)
-		if err != nil {
-			return nil, err
-		}
-
-		return buf, nil
-	}
-	// if inputPath was not piped in we read inputPath from the file located
-	// at a.config.inputPath
-	buf, err := ioutil.ReadFile(inputPath)
-	if err != nil {
-		return nil, err
+func (a *app) createInputReader() (io.Reader, error) {
+	if a.config.inputOnStdin {
+		return os.Stdin, nil
 	}
 
-	return buf, nil
+	return os.Open(a.config.inputPath)
 }
 
-func parseTemplates(templatePaths []string, templateStrings []string) (*template.Template, error) {
+func (a *app) createOutputWriter() (io.Writer, error) {
+	if a.config.outputOnStdout {
+		return os.Stdout, nil
+	}
+
+	return os.Create(a.config.outputPath)
+}
+
+func (a *app) parseTemplates() (*template.Template, error) {
 	// templates can be supplied by filepath or as raw template strings
 	// but not both
-
 	// template files take precedence
-	if len(templatePaths) > 0 {
-		tmpl, err := template.New(filepath.Base(templatePaths[0])).ParseFiles(templatePaths...)
+	if len(a.config.templatePaths) > 0 {
+		tmpl, err := template.New(filepath.Base(a.config.templatePaths[0])).ParseFiles(a.config.templatePaths...)
 		if err != nil {
 			return nil, err
 		}
@@ -85,12 +82,12 @@ func parseTemplates(templatePaths []string, templateStrings []string) (*template
 		return tmpl, nil
 	}
 
-    // if no template files are found then attempt to parse raw template strings
-	if len(templateStrings) > 0 {
+	// if no template files are found then attempt to parse raw template strings
+	if len(a.config.templateStrings) > 0 {
 		var err error
 		var tmpl *template.Template = template.New("base")
 
-		for _, templateString := range templateStrings {
+		for _, templateString := range a.config.templateStrings {
 			tmpl, err = tmpl.Parse(templateString)
 			if err != nil {
 				return nil, err
@@ -100,30 +97,6 @@ func parseTemplates(templatePaths []string, templateStrings []string) (*template
 		return tmpl, nil
 	}
 
-    // if neither files nor strings were found return an error
+	// if neither files nor strings were found return an error
 	return nil, errors.New("no templates found")
-}
-
-func createWriter(writeToStdout bool, outputPath string) (io.Writer, error) {
-	if writeToStdout {
-		return os.Stdout, nil
-	}
-
-	writer, err := os.Create(outputPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return writer, nil
-}
-
-func parseInput(input []byte) (map[string]interface{}, error) {
-	parsed := map[string]interface{}{}
-
-	err := json.Unmarshal(input, &parsed)
-	if err != nil {
-		return nil, err
-	}
-
-	return parsed, nil
 }
